625bcb94074cc2cd57afc928084b1bef78f845cc
André Cianfarani <acianfa@gmail.com>
2018-10-19 10:27:26 +0200

    first commit

---
+++ spec/andre-tools-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MyPackageView from '../lib/andre-tools-view';
+
+describe('MyPackageView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
